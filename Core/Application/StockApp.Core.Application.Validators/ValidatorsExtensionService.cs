64dc80e2d841a64121295ad3a99a04032e8c7628
rgcas <rgcascante@outlook.com>
2025-11-02 20:56:56 -0500

    Feature: Extensión de validadores

---
+++ Core/Application/StockApp.Core.Application.Validators/ValidatorsExtensionService.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StockApp.Core.Application.Validators;
+
+/// <summary>
+/// Clase estatica para agregar validadores
+/// </summary>
+public static class ValidatorsExtensionService
+{
+    /// <summary>
+    /// Método extensivo para agregar validadores
+    /// </summary>
+    /// <param name="services">Servicios</param>
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(ValidatorsExtensionService).Assembly);
+        return services;
+    }
+}
