268c215b93b6622df1b0d10060326e729c5aaa9e
rgcas <rgcascante@outlook.com>
2025-11-02 21:01:10 -0500

    Feature: Helper de strings

---
+++ Shared/StockApp.Shared.Helpers/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace StockApp.Shared.Helpers;
+
+/// <summary>
+/// Clase estática que contiene helpers (extensiones)
+/// para string's
+/// </summary>
+public static class StringHelper
+{
+    /// <summary>
+    /// Metodo mara encriptar cadenas de texto en SHA256
+    /// </summary>
+    public static string ComputeSha256Hash(string rawData)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            var builder = new StringBuilder();
+            foreach (var b in bytes)
+            {
+                builder.Append(b.ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
