884b0c4352f83d5d895b83a198c067e2b3804520
rgcas <rgcascante@outlook.com>
2025-11-02 21:01:24 -0500

    Feature: Dtos de inicio de sesión

---
+++ Core/Application/StockApp.Core.Application.Dtos/Entities/Security/SignInDtos.cs
@@ -0,0 +1,33 @@
+﻿namespace StockApp.Core.Application.Dtos.Entities.Security;
+
+/// <summary>
+/// Dto de entrada para inicio de sesión
+/// </summary>
+public class InSignIn
+{
+    /// <summary>
+    /// Correo del usuario
+    /// </summary>
+    public string? Email { get; set; }    
+    
+    /// <summary>
+    /// Contraseña del usuario
+    /// </summary>
+    public string? Password { get; set; }
+}
+
+/// <summary>
+/// Dto de salida para inicio de sesión
+/// </summary>
+public class OutSignIn
+{
+    /// <summary>
+    /// Token 
+    /// </summary>
+    public string? Token { get; set; }
+    
+    /// <summary>
+    /// Nombre del usuario
+    /// </summary>
+    public string? UserName { get; set; }
+}
