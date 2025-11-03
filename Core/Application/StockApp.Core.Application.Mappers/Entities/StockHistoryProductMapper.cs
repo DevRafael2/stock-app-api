199d04519465346fc0713a768af29fdc30811722
rgcas <rgcascante@outlook.com>
2025-11-02 20:59:39 -0500

    Feature: Creación de perfil de mapeo para stock

---
+++ Core/Application/StockApp.Core.Application.Mappers/Entities/StockHistoryProductMapper.cs
@@ -0,0 +1,22 @@
+﻿using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+using StockApp.Core.Application.Dtos.Entities.Stock;
+using StockApp.Core.Domain.Entities.Stock;
+
+namespace StockApp.Core.Application.Mappers.Entities;
+
+/// <summary>
+/// Perfil de mapeos para el historial de movimientos
+/// </summary>
+public static class StockHistoryProductMapper
+{
+    /// <summary>
+    /// Metodo para agregar las configuraciones de mapeos
+    /// </summary>
+    public static void AddStockHistoryProductMapper()
+    {
+        TypeAdapterConfig.GlobalSettings.NewConfig<StockHistoryProduct, OutStockHistoryProduct>()
+            .Map(e => e.Operation, e => e.Amount <= -1 ? "Salida" : "Entrada")
+            .Map(e => e.Product, e => e.Product == null ? "" : e.Product.Name);
+    }
+}
