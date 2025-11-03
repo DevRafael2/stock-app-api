46e96aa8ff2d2acd6d1ae89398077d1508205a20
rgcas <rgcascante@outlook.com>
2025-11-02 21:03:41 -0500

    Feature: Extensión de persistencia

---
+++ Core/Infraestructure/StockApp.Core.Infraestructure.Persistence/PersistenceServiceExtension.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StockApp.Core.Application.Interfaces.Repositories;
+using StockApp.Core.Infraestructure.Persistence.Context;
+using StockApp.Core.Infraestructure.Persistence.Repositories;
+using StockApp.Shared.Constants;
+
+namespace StockApp.Core.Infraestructure.Persistence;
+
+/// <summary>
+/// Clase estatica para agregar persistencia al
+/// contenedor de dependencias
+/// </summary>
+public static class PersistenceServiceExtension
+{
+    /// <summary>
+    /// Método extensivo para agregar persistencia
+    /// al contenedor de dependencias
+    /// </summary>
+    /// <param name="services">Contenedor de dependencias</param>
+    /// <param name="configuration">Configuración</param>
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<StockAppContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString(ConfigurationConstants.DbConnection));
+        });
+        
+        services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
+        return services;
+    }
+}
