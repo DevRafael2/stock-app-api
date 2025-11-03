4eac931c6b1e93c255fc2db5534c5f1eec54181d
rgcas <rgcascante@outlook.com>
2025-11-02 20:58:05 -0500

    Feature: Creación de contexto de base de datos

---
+++ Core/Infraestructure/StockApp.Core.Infraestructure.Persistence/Context/StockAppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StockApp.Core.Infraestructure.Persistence.Context;
+
+/// <summary>
+/// Contexto de base de datos 
+/// </summary>
+/// <param name="options">Opciones de contexto de base de datos</param>
+public class StockAppContext(DbContextOptions<StockAppContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(StockAppContext).Assembly);
+    }
+}
