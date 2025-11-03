337cf7ddde3152da491a2b591c459bda4311d22d
rgcas <rgcascante@outlook.com>
2025-11-02 20:57:10 -0500

    Feature: Creación de extensión de casos de uso

---
+++ Core/Application/StockApp.Core.Application.UseCases/UseCasesServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace StockApp.Core.Application.UseCases;
+
+/// <summary>
+/// Clase estática para agregar casos de uso
+/// </summary>
+public static class UseCasesServiceExtension
+{
+    /// <summary>
+    /// Método para agregar casos de uso al contenedor
+    /// de dependencias
+    /// </summary>
+    /// <param name="services">Contenedor de dependencias</param>
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddMediatR(e => e.RegisterServicesFromAssembly(typeof(UseCasesServiceExtension).Assembly));
+        return services;
+    }
+}
