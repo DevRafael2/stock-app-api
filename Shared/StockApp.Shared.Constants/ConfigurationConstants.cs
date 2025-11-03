47d102bfd71628f70dcb2ffbe0ed9b19ac9be3c7
rgcas <rgcascante@outlook.com>
2025-11-02 21:03:00 -0500

    Feature: Constantes de configuración

---
+++ Shared/StockApp.Shared.Constants/ConfigurationConstants.cs
@@ -0,0 +1,27 @@
+﻿namespace StockApp.Shared.Constants;
+
+/// <summary>
+/// Constantes de configuración
+/// </summary>
+public static class ConfigurationConstants
+{
+    /// <summary>
+    /// Audiencia de token
+    /// </summary>
+    public static string TokenAudience => "tokenSettings:audience";
+    
+    /// <summary>
+    /// Issuer del token 
+    /// </summary>
+    public static string TokenIssuer => "tokenSettings:issuer";
+    
+    /// <summary>
+    /// Security key del token
+    /// </summary>
+    public static string TokenSecurityKey => "tokenSettings:key";
+    
+    /// <summary>
+    /// Conexión a la base de datos
+    /// </summary>
+    public static string DbConnection => "defaultConnection";
+}
