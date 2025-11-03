126335ad7dfcb024484971e42e843d53f9953f4f
rgcas <rgcascante@outlook.com>
2025-11-02 21:02:36 -0500

    Feature: Entidad base root

---
+++ Core/Domain/StockApp.Core.Domain.Primitives/EntityRoot.cs
@@ -0,0 +1,13 @@
+﻿namespace StockApp.Core.Domain.Primitives;
+
+/// <summary>
+/// Entidad base
+/// </summary>
+/// <typeparam name="TId">Tipo de id de la entidad</typeparam>
+public class EntityRoot<TId>
+{
+    /// <summary>
+    /// Id del registro
+    /// </summary>
+    public TId? Id { get; set; }
+}
