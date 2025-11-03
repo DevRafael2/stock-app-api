751ae4a720827103b891a357e3ee62eb4418841c
rgcas <rgcascante@outlook.com>
2025-11-02 21:03:33 -0500

    Feature: Extensión de mapeos

---
+++ Core/Application/StockApp.Core.Application.Mappers/MappersServiceExtension.cs
@@ -0,0 +1,24 @@
+﻿using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+using StockApp.Core.Application.Mappers.Entities;
+using StockApp.Core.Domain.Entities.Stock;
+
+namespace StockApp.Core.Application.Mappers;
+
+/// <summary>
+/// Clase extensiva para agregar mappers
+/// </summary>
+public static class MappersServiceExtension
+{
+    /// <summary>
+    /// Metodo para agregar maps
+    /// </summary>
+    /// <param name="services">Contenedor de dependencias</param>
+    public static IServiceCollection AddMappers(this IServiceCollection services)
+    {
+        StockHistoryProductMapper.AddStockHistoryProductMapper();
+        services.AddSingleton(TypeAdapterConfig.GlobalSettings);
+        services.AddMapster();
+        return services;
+    }
+}
