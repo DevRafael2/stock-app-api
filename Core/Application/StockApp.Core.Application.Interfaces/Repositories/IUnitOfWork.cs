6abd2043676a4802ff937ec56aa4d39422d50821
rgcas <rgcascante@outlook.com>
2025-11-02 20:55:15 -0500

    Feature: Creación de interfaz de unidad de trabajo

---
+++ Core/Application/StockApp.Core.Application.Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using StockApp.Core.Domain.Primitives;
+
+namespace StockApp.Core.Application.Interfaces.Repositories;
+
+/// <summary>
+/// Unidad de trabajo
+/// </summary>
+public interface IUnitOfWork
+{
+    /// <summary>
+    /// Metodo que obtiene un repositorio
+    /// </summary>
+    /// <typeparam name="TEntity">Entidad del repositorio</typeparam>
+    /// <typeparam name="TId">Tipo de id de la entidad</typeparam>
+    public IRepository<TEntity, TId> GetRepository<TEntity, TId>() where TEntity : EntityRoot<TId>, new();
+    /// <summary>
+    /// Metodo que obtiene repositorio no generico
+    /// </summary>
+    /// <typeparam name="TRepository">Tipo del repositorio</typeparam>
+    /// <typeparam name="TEntity">Entidad</typeparam>
+    /// <typeparam name="TId">Tipo de id</typeparam>
+    /// <returns></returns>
+    public TRepository GetRepository<TRepository, TEntity, TId>()
+        where TRepository : notnull, IRepository<TEntity, TId>
+        where TEntity : EntityRoot<TId>, new();
+    /// <summary>
+    /// Metodo para indicar que los cambios han sido completados
+    /// </summary>
+    public Task CompleteAsync();
+}
