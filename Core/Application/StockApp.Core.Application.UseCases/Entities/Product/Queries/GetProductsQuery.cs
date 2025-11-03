f2fc37faea6b98941150ecc9cc4bd2eaae608770
rgcas <rgcascante@outlook.com>
2025-11-02 21:03:14 -0500

    Feature: Query para obtener productos

---
+++ Core/Application/StockApp.Core.Application.UseCases/Entities/Product/Queries/GetProductsQuery.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using MapsterMapper;
+using StockApp.Core.Application.Dtos.Entities.Stock;
+using StockApp.Core.Application.Interfaces.Repositories;
+using StockApp.Core.Application.UseCases.Base.Queries;
+using StockApp.Core.Domain.Primitives;
+
+namespace StockApp.Core.Application.UseCases.Entities.Product.Queries;
+
+using Domain.Entities.Stock;
+
+/// <summary>
+/// Request QueryParams para productos
+/// </summary>
+public class GetProductsRequest : QueryParams<Product, OutProduct>
+{
+    public override Expression<Func<Product, bool>> GetWhereExpression() => e => true;
+
+    public override Expression<Func<Product, Product>> GetSelectExpression() => e => e;
+}
+
+/// <summary>
+/// Query para obtener productos
+/// </summary>
+/// <param name="unitOfWork">Unidad de trabajo</param>
+/// <param name="mapper">Mapster</param>
+public class GetProductsQuery(IUnitOfWork unitOfWork, IMapper mapper) : 
+    BaseGetQuery<Product, int, GetProductsRequest, OutProduct>(unitOfWork, mapper);
